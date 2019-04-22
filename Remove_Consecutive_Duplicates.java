70c13c4c7e5ff00d758d19cab4cae4b507c408da
shubham91939 <48523144+shubham91939@users.noreply.github.com>
2019-04-22 11:20:25 +0530

    Add files via upload

---
+++ Remove_Consecutive_Duplicates.java
@@ -0,0 +1,18 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Remove_Consecutive_Duplicates {
+	
+//	public static void removeDuplicates(String)
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+		
+	}
+
+}
